96399aaadb85a0e7864cf5de1a76e00ca077abf2
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-05-15 10:35:11 +0530

    Add files via upload

---
+++ Array/LengthOfArray.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class LengthOfArray {
+    public static void main(String[] args) {
+        int marks[] = new int[40];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Length of an Array: " +marks.length);
+    }
+}
