149e07d7402c139cd8bd8fcd079387b41b21bc2b
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-01 12:03:01 +0530

    Add files via upload

---
+++ Array/LargestElement2.java
@@ -0,0 +1,16 @@
+public class LargestElement2 {
+    public static void main(String[] args) {
+        int arr1[] = { 4, 8, 3, 7, 9, 2 };
+        System.out.println("The Largest Number is: " +largest_number(arr1));
+    }
+
+    public static int largest_number(int arr[]) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
