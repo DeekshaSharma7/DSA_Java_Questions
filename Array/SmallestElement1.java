58ec2713e31d6315afa97b9c1eddceb4fe91028f
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-01 10:33:28 +0530

    Add files via upload

---
+++ Array/SmallestElement1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SmallestElement1 {
+    public static int sort(int arr[]) {
+        Arrays.sort(arr);
+        return arr[0];
+    }
+    public static void main(String[] args) {
+        int arr1[] = { 5, 9, 7, 3, 8 };
+        System.out.println("The smallest Element is: " +sort(arr1));
+    }
+}
