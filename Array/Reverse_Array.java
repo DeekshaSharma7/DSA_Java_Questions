7997cba05a6d46875b75331423d28bd0e2584154
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-01 13:26:17 +0530

    Add files via upload

---
+++ Array/Reverse_Array.java
@@ -0,0 +1,32 @@
+public class Reverse_Array {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        System.out.println("The Original Array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+        reverse(arr);
+
+        System.out.println();
+        System.out.println("The Reverse Array is: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +" ");
+        }
+    }
+
+    public static void reverse(int arr[]) {
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
