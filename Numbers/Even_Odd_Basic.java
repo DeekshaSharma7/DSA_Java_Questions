bc15333ecc98f0998f132ddb0be287a90f5ce120
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-02 12:56:42 +0530

    Add files via upload

---
+++ Numbers/Even_Odd_Basic.java
@@ -0,0 +1,14 @@
+public class Even_Odd {
+    public static void main(String[] args) {
+        int n = 5;
+        if (remainder(n) == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
+
+    public static int remainder(int n) {
+        return (n % 2);
+    }
+}
