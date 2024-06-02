2372338cf967ad333a46777e39f26272b842520f
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-02 13:26:53 +0530

    Add files via upload

---
+++ Numbers/Reverse.java
@@ -0,0 +1,16 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int n = 12345;
+        System.out.println("The Reverse Number is: " +solve(n));
+    }
+
+    public static int solve(int n) {
+        int rev = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            rev = rev * 10 + rem;
+            n = n / 10;
+        }
+        return rev;
+    }
+}
