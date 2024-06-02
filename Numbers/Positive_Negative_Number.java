2cf38c7ff77b104a591ff4cde8bedbd2c503d6d2
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-02 13:39:36 +0530

    Add files via upload

---
+++ Numbers/Positive_Negative_Number.java
@@ -0,0 +1,14 @@
+public class Positive_Negative_Number {
+    public static void main(String[] args) {
+        int n = -15;
+        solve(n);
+    }
+
+    public static void solve(int n) {
+        if (n > 0) {
+            System.out.println("The number " +n+ " is Positive");
+        } else {
+            System.out.println("The Number " +n+ " is Negative");
+        }
+    }
+}
