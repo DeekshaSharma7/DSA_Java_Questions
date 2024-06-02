d049220ac8d2369fbc74579b0de6c1650f344e2c
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-02 13:09:39 +0530

    Add files via upload

---
+++ Numbers/Greatest_2Number.java
@@ -0,0 +1,7 @@
+public class Greatest_2Number {
+    public static void main(String[] args) {
+        int n1 = 4521;
+        int n2 = 4513;
+        System.out.println("The Greatest Number is: " +Math.max(n1, n2));
+    }
+}
