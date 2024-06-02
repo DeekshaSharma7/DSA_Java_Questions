2247b18397481d6f3578433f965ad48654043447
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-02 13:19:07 +0530

    Add files via upload

---
+++ Numbers/Greatest_3Number2.java
@@ -0,0 +1,15 @@
+public class Greatest_3Number2 {
+    public static void main(String[] args) {
+        int n1 = 11;
+        int n2 = 22;
+        int n3 = 16;
+
+        if ((n1 > n2) && (n1>n3)) {
+            System.out.println("The Greatest Number is: "+n1);
+        } else if ((n2 > n1) && (n2 > n3)) {
+            System.out.println("The Greatest Number is: " +n2);
+        } else {
+            System.out.println("The Greatest Number is: " +n3);
+        }
+    }
+}
