2cbf6ca2ed79f77194e7412eed9fc292f93e4775
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-02 12:48:29 +0530

    Add files via upload

---
+++ Numbers/Sum_of_Natural_Number.java
@@ -0,0 +1,14 @@
+public class Sum_of_Natural_Number {
+    public static void main(String[] args) {
+        solve(5);
+        solve(6);
+    }
+
+    public static void solve(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of the Natural Number is: "+sum);
+    }
+}
