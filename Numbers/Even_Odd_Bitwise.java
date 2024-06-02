ff243a31ed93579914b4edca7e285c5b0793fdbd
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-02 13:03:03 +0530

    Add files via upload

---
+++ Numbers/Even_Odd_Bitwise.java
@@ -0,0 +1,18 @@
+public class Even_Odd_Bitwise {
+    public static void main(String[] args) {
+        int n = 6;
+        if (solve(n) == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+    
+    public static int solve(int n) {
+        if ((n & 1) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
