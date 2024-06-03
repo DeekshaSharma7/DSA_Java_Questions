9cdcf8cc533e562d43f7f09b683ca734ba20ca4b
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-03 12:32:39 +0530

    Add files via upload

---
+++ Numbers/Prime_InRange1.java
@@ -0,0 +1,28 @@
+public class Prime_InRange1 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 15;
+        Prime_in_given_Range(a, b);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void Prime_in_given_Range(int a, int b) {
+        for (int i = a; i <= b; i++) {
+            if(isPrime(i)){
+                System.out.println(i+" ");
+            }
+        }
+    }
+}
