9d70886aeddb9cdf9c59be513f7fd4132cac5675
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-03 12:06:34 +0530

    Add files via upload

---
+++ Numbers/Prime_Number.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Prime_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the Number: ");
+        int n = sc.nextInt();
+
+        if (isPrime(n)) {
+            System.out.println(n+ " is a Prime Number.");
+        } else {
+            System.out.println(n+ " is not a Prime Number.");
+        }
+    }
+
+    public static boolean isPrime(int n){
+        if (n <= 1) {
+            return false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
