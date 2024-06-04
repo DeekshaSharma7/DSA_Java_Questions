e36df6c2185f450a86e1be51879fbeec56044635
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-04 11:55:26 +0530

    Add files via upload

---
+++ Numbers/Duplicate.java
@@ -0,0 +1,22 @@
+
+public class Duplicate {
+    public static void main(String[] args) {
+        int arr[] = { 1, 1, 2, 3, 4, 4, 4, 5, 7 };
+        int k = removeDuplicate(arr);
+        System.out.println("The Array after removing duplicate Elements: "+k);
+        for (int i = 0; i < k; i++) {
+            System.out.println(arr[i] +" ");
+        }
+    }
+
+    public static int removeDuplicate(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i+1;
+    }
+}
