ec55c922005a79977cd943ff51cac74869db88f0
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-08 14:37:23 +0530

    Add files via upload

---
+++ Pattern/Print_Character.java
@@ -0,0 +1,13 @@
+public class Print_Character {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
