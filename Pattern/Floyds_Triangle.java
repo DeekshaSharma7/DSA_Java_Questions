dc51a382381ee403f41d6b87b8fcbfa1bb1d3769
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-09 11:16:20 +0530

    Add files via upload

---
+++ Pattern/Floyds_Triangle.java
@@ -0,0 +1,15 @@
+public class Floyds_Triangle {
+    public static void triangle(int n) {
+        int a = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a + " ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        triangle(4);
+    }
+}
