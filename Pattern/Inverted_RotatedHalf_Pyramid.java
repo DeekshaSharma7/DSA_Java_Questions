ca7745a2e1bc0a3fea222c219ad5e776c72fcfbd
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-09 10:56:39 +0530

    Add files via upload

---
+++ Pattern/Inverted_RotatedHalf_Pyramid.java
@@ -0,0 +1,16 @@
+public class Inverted_RotatedHalf_Pyramid {
+    public static void Half_pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Half_pyramid(4);
+    }
+}
