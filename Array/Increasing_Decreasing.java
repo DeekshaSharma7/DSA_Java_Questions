bf024d8819b9c131799eb41e5e246e59a1a7d974
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-12 15:34:05 +0530

    Add files via upload

---
+++ Array/Increasing_Decreasing.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Increasing_Decreasing {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 7, 5, 6, 4 };
+        int n = arr.length;
+        Arrays.sort(arr);
+        for (int i = 0; i < n / 2; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        for (int i = n - 1; i >= n / 2; i--) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
