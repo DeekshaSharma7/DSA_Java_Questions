912f12853761be990dbddc00a9f64cd865aa73ca
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-12 17:13:38 +0530

    Add files via upload

---
+++ Array/RemoveDuplicate_Sorted.java
@@ -0,0 +1,21 @@
+public class RemoveDuplicate_Sorted {
+    public static void main(String[] args) {
+        int arr[] = { 1, 1, 2, 2, 2, 3, 3, 3, 4, 4 };
+        int k = removeDupl(arr);
+        System.out.println("The array after removing: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] +" ");
+        }
+    }
+
+    public static int removeDupl(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+}
