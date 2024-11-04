8a52c65bb487bcc359101a6dc7c7f7cb89238e4d
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-11-04 15:31:21 +0000

    Array

---
+++ 1_Arrays/ArraysIntro.java
@@ -0,0 +1,12 @@
+//package Arrays;
+public class ArraysIntro {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        int numbers[] = {1,2,5};
+        String fruits[] = {"apple", "banana", "grapes"};
+
+        System.out.println(marks[0]);
+        System.out.println(numbers[0]);
+        System.out.println(fruits[1]);
+    }
+}
