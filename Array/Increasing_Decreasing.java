1244d4066680caae0001a0720cc4f6951d96e30e
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2025-01-01 17:08:29 +0530

    Increasing_Decreasing.java

--- Array/Increasing_Decreasing.java
+++ Array/Increasing_Decreasing.java
@@ -1,21 +1,52 @@
+// import java.util.*;
+
+// public class Increasing_Decreasing {
+//     public static void main(String[] args) {
+//         int arr[] = { 1, 2, 7, 5, 6, 4 };
+//         int n = arr.length;
+//         Arrays.sort(arr);
+//         for (int i = 0; i < n / 2; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//         for (int i = n - 1; i >= n / 2; i--) {
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+
+// // ----------------------------------------------------------------------
+
+// //     OUTPUT:
+// //     1 2 4 7 6 5 
+
+
+
+
 import java.util.*;
 
 public class Increasing_Decreasing {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 7, 5, 6, 4 };
         int n = arr.length;
-        Arrays.sort(arr);
+        
+        // Custom sorting function
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        
         for (int i = 0; i < n / 2; i++) {
             System.out.print(arr[i] + " ");
         }
         for (int i = n - 1; i >= n / 2; i--) {
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         }
     }
 }
-
-
-// ----------------------------------------------------------------------
-
-//     OUTPUT:
-//     1 2 4 7 6 5 
