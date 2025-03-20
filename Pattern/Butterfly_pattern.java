6d755f1a4c91947b05c8545c97b38d063e9750d3
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2025-03-20 23:02:53 +0530

    Update Butterfly_pattern.java

--- Pattern/Butterfly_pattern.java
+++ Pattern/Butterfly_pattern.java
@@ -13,6 +13,7 @@ public class Butterfly_pattern {
             System.out.println();
         }
 
+
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
